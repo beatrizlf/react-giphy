a6abdd60649940e7407ea1b34d51cbdba5e6805e
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-04-07 19:00:12 -0300

    gif list props

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Gif from './gif.jsx';
 
-class GifList extends Component {
-  renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
-    }
-
-  render() {
-    return (
-      <div className="gif-lis">
-        {this.renderList()}
-      </div>
-    );
-  }
-}
+const GifList = (props) => {
+  return (
+    <div className="gif-list">
+      {props.gifs.map(gif => <Gif id={gif} key={gif} selectGif={props.selectGif} />)}
+    </div>
+  );
+};
 
 export default GifList;
